d3179a4f376bf08d900a8c486ee25f8a6eaad343
Дмитрий Жур <tehzienergy@gmail.com>
2016-10-19 22:57:31 +0300

    add map & address

--- build/js/script.js
+++ build/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 
 
 
+
 $(".slider").slick({infinite:!1});
 
 $(function(){$(window).bind("resize",function(){resizeMe()}).trigger("resize")});var resizeMe=function(){var i=750,e=10,n=$(window).width(),r=n/i,s=e*r;$("html").css("font-size",s)};})
