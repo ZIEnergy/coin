a27dfa9f41415abb7fb89af32a4ca6a558f60a2a
Дмитрий Жур <tehzienergy@gmail.com>
2016-10-20 22:30:17 +0300

    update coin

--- build/js/script.js
+++ build/js/script.js
@@ -3,7 +3,11 @@ $(document).ready(function(){
 $('.coin__button').click(function(e) {
   e.preventDefault();
   $(this).toggleClass('coin__button--spin');
+  $('.coin__discount').toggleClass('coin__discount--fading');
   $('.coin__discount').toggleClass('coin__discount--active');
+  setInterval(function(){
+    $('.coin__discount').toggleClass('coin__discount--fading');
+  }, 500);
   setTimeout(function() {
     $('.coin__message').slideToggle('fast');
     $('html, body').animate({
--- src/blocks/coin/coin.js
+++ src/blocks/coin/coin.js
@@ -2,9 +2,9 @@ $('.coin__button').click(function(e) {
   e.preventDefault();
   $(this).toggleClass('coin__button--spin');
   $('.coin__discount').toggleClass('coin__discount--active');
-//  setInterval(function(){
-//    $('.coin__discount').toggle();
-//  }, 500);
+  setInterval(function(){
+    $('.coin__discount').toggleClass('coin__discount--fading');
+  }, 500);
   setTimeout(function() {
     $('.coin__message').slideToggle('fast');
     $('html, body').animate({
