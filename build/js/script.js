b60e0abce8c9e193c8a9fcc87810b9cbd8740c6a
Дмитрий Жур <tehzienergy@gmail.com>
2016-10-24 21:24:39 +0300

    fix animation

--- build/js/script.js
+++ build/js/script.js
@@ -1,20 +1,6 @@
 $(document).ready(function(){
 
-$('.coin__button').click(function(e) {
-  e.preventDefault();
-  $(this).toggleClass('coin__button--spin');
-  $('.coin__discount').toggleClass('coin__discount--fading');
-  $('.coin__discount').toggleClass('coin__discount--active');
-  setInterval(function(){
-    $('.coin__discount').toggleClass('coin__discount--fading');
-  }, 500);
-  setTimeout(function() {
-    $('.coin__message').slideToggle('fast');
-    $('html, body').animate({
-        scrollTop: $("#prize").offset().top
-    }, 500);
-  }, 4000);
-});
+function discount(){timerId=setInterval(function(){$(".coin__discount").addClass("coin__discount--fading");var n=discountPrice[Math.floor(Math.random()*discountPrice.length)];$(".coin__button-number").html(n),$(".coin__message-discount").html("100"),setTimeout(function(){$(".coin__discount").removeClass("coin__discount--fading")},62)},500)}var timerId=0,discountPrice=["50","100","150","200","250","300","1000","2000"];$(".coin__button").click(function(n){n.preventDefault(),$(this).toggleClass("coin__button--spin"),$(".coin__discount").toggleClass("coin__discount--active"),discount(),setTimeout(function(){$(".coin__message").slideToggle("fast"),$("html, body").animate({scrollTop:$("#prize").offset().top},500),clearInterval(timerId),$(".coin__button-number").html("100"),$(this).removeClass("coin__button--spin"),$(".coin__discount").addClass("coin__discount--fading")},4e3)});
 
 
 $(".slider__item").click(function(){$(".popup").fadeIn("fast"),$("body").addClass("body--blocked")}),$(".popup__button").click(function(){$(".popup").fadeOut("fast"),$("body").removeClass("body--blocked")});
--- src/blocks/coin/coin.js
+++ src/blocks/coin/coin.js
@@ -1,14 +1,32 @@
+var timerId = 0;
+  
+var discountPrice = ['50', '100', '150', '200', '250', '300', '1000', '2000'];
+
+function discount() {
+  timerId = setInterval(function () {
+    $('.coin__discount').addClass('coin__discount--fading');
+    var discountItem = discountPrice[Math.floor(Math.random()*discountPrice.length)];
+    $('.coin__button-number').html(discountItem);
+    $('.coin__message-discount').html('100');
+    setTimeout(function () {
+      $('.coin__discount').removeClass('coin__discount--fading');
+    }, 62);
+  }, 500);
+};
+
 $('.coin__button').click(function(e) {
   e.preventDefault();
   $(this).toggleClass('coin__button--spin');
   $('.coin__discount').toggleClass('coin__discount--active');
-  setInterval(function(){
-    $('.coin__discount').toggleClass('coin__discount--fading');
-  }, 500);
+  discount();
   setTimeout(function() {
     $('.coin__message').slideToggle('fast');
     $('html, body').animate({
         scrollTop: $("#prize").offset().top
     }, 500);
+    clearInterval(timerId);
+    $('.coin__button-number').html('100');
+    $(this).removeClass('coin__button--spin');
+    $('.coin__discount').addClass('coin__discount--fading');
   }, 4000);
 });
